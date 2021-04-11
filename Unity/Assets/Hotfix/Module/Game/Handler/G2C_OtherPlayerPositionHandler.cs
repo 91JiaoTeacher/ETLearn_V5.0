3e60e24d1492569d7e65cf08d14d6b6395db5e96
91JiaoMan <1292366529@qq.com>
2021-04-12 06:00:35 +0800

    修复问题

--- Unity/Assets/Hotfix/Module/Game/Handler/G2C_OtherPlayerPositionHandler.cs
+++ Unity/Assets/Hotfix/Module/Game/Handler/G2C_OtherPlayerPositionHandler.cs
@@ -50,17 +50,22 @@ namespace ETHotfix
                     }
                 }
 
+                PlayerInfoComponent playerInfoComponent = Game.Scene.GetComponent<PlayerInfoComponent>();
+
                 //同步子弹数量
                 BulletInfo[] bulletInfos = message.Bullets.array;
                 for (int i = 0; i < bulletInfos.Length; i++)
                 {
                     BulletInfo bulletInfo = bulletInfos[i];
 
-                    CubeBullet cubeBullet = CubeBulletFactory.CreateCubeBullet();
-                    cubeBullet.SyncBullet(new Vector3(bulletInfo.PositionX, bulletInfo.PositionY, bulletInfo.PositionZ),
-                        new Quaternion(bulletInfo.RotationX, bulletInfo.RotationY, bulletInfo.RotationZ, bulletInfo.RotationW),
-                        new Vector3(bulletInfo.VelocityX, bulletInfo.VelocityY, bulletInfo.VelocityZ));
-
+                    //不是自己的子弹才需要创建同步
+                    if (bulletInfo.Account != playerInfoComponent.account)
+                    {
+                        CubeBullet cubeBullet = CubeBulletFactory.CreateCubeBullet();
+                        cubeBullet.SyncBullet(new Vector3(bulletInfo.PositionX, bulletInfo.PositionY, bulletInfo.PositionZ),
+                            new Quaternion(bulletInfo.RotationX, bulletInfo.RotationY, bulletInfo.RotationZ, bulletInfo.RotationW),
+                            new Vector3(bulletInfo.VelocityX, bulletInfo.VelocityY, bulletInfo.VelocityZ));
+                    }
                 }
 
             }
--- Unity/Assets/Hotfix/Module/Game/PlayerCube/Component/CubeBulletManagerComponent.cs
+++ Unity/Assets/Hotfix/Module/Game/PlayerCube/Component/CubeBulletManagerComponent.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace ETHotfix
 {
     /// <summary>
-    /// 字典的管理组件，主要用作父物体
+    /// 子弹的管理组件，主要用作父物体
     /// </summary>
     public class CubeBulletManagerComponent : Component
     {
